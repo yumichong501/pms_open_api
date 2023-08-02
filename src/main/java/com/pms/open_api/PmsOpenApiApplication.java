bdb0e7f53e815b43d109283bbf77e5dc90fa7204
hanzhilu <hanzhilu@pharmaoryx.com>
2023-08-02 13:02:45 +0800

    init

---
+++ src/main/java/com/pms/open_api/PmsOpenApiApplication.java
@@ -0,0 +1,13 @@
+package com.pms.open_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsOpenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsOpenApiApplication.class, args);
+    }
+
+}
